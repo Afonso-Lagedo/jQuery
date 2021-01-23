fa73d2269d798ca9148f61d78523cad896f869e8
Afonso-cmd <afonso.ur@gmail.com>
2021-01-22 22:00:24 -0300

    jQuery efeito fade

--- script_jquery.js
+++ script_jquery.js
@@ -2,8 +2,12 @@
 $(document).ready(function(){//verificar  se a página foi lida
 	$('#verde').click(function(){
 		$('h1').css("color","green");
+		$('h1').fadeOut();// "um hide" com efeito
+		$('h1').delay(3000);//pausa no javaScript 1000=1s
+		$('h1').fadeIn('slow');//slow mais suave  'fast' para mais rápido
 	});
 	$('#azul').click(function(){
 		$('h1').css("color","blue");
+		$('h1').hide();
 	});
 });
