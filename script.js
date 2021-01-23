2ced99fda415545a8354eec2ceb57c94b29969df
Afonso-cmd <afonso.ur@gmail.com>
2021-01-22 21:00:58 -0300

    operações matemáticas

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-var mensagem = "Olá mundo";
-alert(mensagem);
+var a = 2;
+var b = 3;
+
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
