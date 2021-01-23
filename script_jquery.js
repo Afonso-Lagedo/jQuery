9daa9e9a505e5d841e6e8beeef411b4b2646d6e1
Afonso-cmd <afonso.ur@gmail.com>
2021-01-22 23:02:41 -0300

    jQuery adicionando/removendo classe

--- script_jquery.js
+++ script_jquery.js
@@ -1,10 +1,13 @@
 // $(seletor).ação();
 $(document).ready(function(){//verificar  se a página foi lida
 	$('#verde').click(function(){
-		$('h1').css("color","green");
-		$('h1').fadeOut();// "um hide" com efeito
-		$('h1').delay(3000);//pausa no javaScript 1000=1s
-		$('h1').fadeIn('slow');//slow mais suave  'fast' para mais rápido
+		$('h1')
+			.css("color","green")
+			.fadeOut()// "um hide" com efeito
+			.delay(3000)//pausa no javaScript 1000=1s
+			.fadeIn('slow')//slow mais suave  'fast' para mais rápido
+			.addClass('teste');
+		
 	});
 	$('#azul').click(function(){
 		$('h1').css({color:'blue', fontSize:'100px'});//css encadeado
